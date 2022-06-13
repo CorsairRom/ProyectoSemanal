f95644663e3a6a32bdce7150fb5c34806432d3d8
CorsairRom <ric.romero@duocuc.cl>
2022-06-13 13:34:11 -0400

    cambio de carpetas

--- ProyectoSemestral/entorno/Scripts/activate.bat
+++ ProyectoSemestral/entorno/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\richa\Desktop\informatica2\Django\ProyectoSemestral\entorno
+set VIRTUAL_ENV=C:\Django\ingSoftware\nuevoProyecto\entorno
 
 if not defined PROMPT set PROMPT=$P$G
 
